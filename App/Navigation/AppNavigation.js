df4aa44c4dbae0175e7ebaec46baaab6e31e371b
Bicky Eric Kantona <bickyeric@ymail.com>
2019-10-19 12:30:36 +0700

    grid

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,19 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import BlankScreen from '../Containers/BlankScreen'
+import SearchResultScreen from '../Containers/SearchResultScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  BlankScreen: { screen: BlankScreen },
+  SearchResultScreen: { screen: SearchResultScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'SearchResultScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
